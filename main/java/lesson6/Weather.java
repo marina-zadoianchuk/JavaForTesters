a809f1ce305fc9f9dcc79173cb52ec455fb6a892
Marina <93602034+marina-zadoianchuk@users.noreply.github.com>
2022-06-16 13:18:03 +0300

    Add files via upload

---
+++ main/java/lesson6/Weather.java
@@ -0,0 +1,35 @@
+package lesson6;
+
+import okhttp3.*;
+import java.io.IOException;
+
+public class Weather {
+
+
+    public static void main(String[] args) throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        String apiKeyQueryParam = "apikey";
+        String metricQueryParam = "metric";
+        String apiKey = "A17O4On71vceDLGIkJmh74y3otL2FgAy";
+        String metric = "true";
+        HttpUrl httpUrl = new HttpUrl.Builder()
+                .scheme("http")
+                .host("dataservice.accuweather.com")
+                .addPathSegment("forecasts")
+                .addPathSegment("v1")
+                .addPathSegment("daily")
+                .addPathSegment("5day")
+                .addPathSegment("294021")
+                .addQueryParameter(apiKeyQueryParam, apiKey)
+                .addQueryParameter(metricQueryParam, metric)
+                .build();
+
+        Request requestCreateWeather = new Request.Builder()
+                .url(httpUrl)
+                .build();
+
+        Response createWeatherResponse = okHttpClient.newCall(requestCreateWeather).execute();
+
+        System.out.println(createWeatherResponse.body().string());
+    }
+}
